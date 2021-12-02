bf8bea2fae9dabc012720b33345ed9f02e6c3004
Googol <57511576+googol88@users.noreply.github.com>
2021-12-02 17:54:43 +0000

    feat: complete day 1

--- days/Day1/Day1.java
+++ days/Day1/Day1.java
@@ -1,13 +1,33 @@
 public class Day1 extends Day {
-    Day1() {
-        inputFile = "days/Day1/input1.txt";
-    }
+  Day1() {
+    inputFile = "days/Day1/input1.txt";
+  }
 
-    public void part1() {
-        // Good luck!
+  public void part1() {
+    String[] lines = this.getInput().split("\n");
+    int count = 0;
+    int num, prev;
+    for (int i = 1; i < lines.length; i++) {
+      num = Integer.parseInt(lines[i]);
+      prev = Integer.parseInt(lines[i-1]);
+      if(num > prev) {
+        count++;
+      }
     }
+    System.out.println(String.format("Part 1: %d", count));
+  }
 
-    public void part2() {
-        
+  public void part2() {
+    String[] lines = this.getInput().split("\n");
+    int count = 0;
+    int num, prev;
+    for (int i = 3; i < lines.length; i++) {
+      num = Integer.parseInt(lines[i]);
+      prev = Integer.parseInt(lines[i-3]);
+      if(num > prev) {
+        count++;
+      }
     }
+    System.out.println(String.format("Part 1: %d", count));
+  }
 }
